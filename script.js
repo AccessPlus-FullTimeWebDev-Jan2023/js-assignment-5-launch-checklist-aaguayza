3c32637249e430d5db515edebe1e97ba36d03c8b
Tony <adaguayza@gmail.com>
2023-02-21 11:13:55 -0600

    more edits

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ window.addEventListener("load", function() {
         }
         });
 
-        console.log(myFetch())
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
