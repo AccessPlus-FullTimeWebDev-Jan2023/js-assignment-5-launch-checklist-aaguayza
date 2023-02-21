8159104faa914b55c9421554654192c3e770cb11
Tony <adaguayza@gmail.com>
2023-02-21 10:33:54 -0600

    addDestination function is updated

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,17 +3,17 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   /*
+   document.getElementById("missionTarget").innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name} </li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance} </li>
+                    <li>Number of Moons: ${moons} </li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+                `
 }
 
 function validateInput(testInput) {
@@ -55,14 +55,16 @@ async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-        response.json().then(function(json){
+        return response.json()/*.then(function(json){
             return json
-        })
+        })*/
         });
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let chosenPlanet = planets[Math.floor(Math.random()*planets.length)]
+    return chosenPlanet
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
